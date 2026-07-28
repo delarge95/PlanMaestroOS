6b53ae099157ae30ed78963ed507311e583d4907
delarge95 <awoodcocks@unal.edu.co>
2026-07-28 18:44:42 -0500

    ci: Add GitHub Actions workflow for automatic deployment to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://delarge95.github.io",
+  base: "/PlanMaestroOS/",
   integrations: [react()],
   devToolbar: {
     enabled: false
